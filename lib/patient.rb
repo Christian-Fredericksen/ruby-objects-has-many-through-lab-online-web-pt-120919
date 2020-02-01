c5fe4301f46ce6da2bd7125c134be7641ea7f896
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-02-01 19:48:50 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,24 +1,26 @@
+require 'pry'
 class Patient
-
-  attr_reader :name, :appointment, :doctor
-
+  attr_accessor :name, :appointment, :doctor
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
-
-  def new_appointment(doctor, date)
-    Appointment.new(self, doctor, date)
+  def add_appointment(appointment)
+    @appointments << appointment
+    appointment.patient = self
   end
-
+  def self.all
+    @@all
+  end
   def appointments
-    Appointment.all.select do |appointment|
-      appointment.patient == self
-    end
+    # binding.pry
+    Appointment.all.select  {|appointment| appointment.patient == self}
   end
-
   def doctors
-    Appointment.all.map do |appointment|
-      appointment.doctor
-    end
+    appointments.collect {|appointment| appointment.doctor}
+  end
+  def new_appointment(doctor, date)
+    Appointment.new(date,self,doctor)
   end
 end
